2a29cd955c306943c089bbf3f53e5568a7a4184e
Yurok <predbannikov.yurii@gmail.com>
2022-01-16 10:18:24 +0300

    fix stage debug, issu node contain not valid OrderLevel

--- exchange-cli/main.c
+++ exchange-cli/main.c
@@ -5,6 +5,18 @@
 #include "hashtable.h"
 
 
+void heightTreeOid(NodeOID* tree, int lvl, int* max) {
+    if (*max < lvl)
+        *max = lvl;
+    if (tree->left != NILOID) {
+        heightTreeOid(tree->left, lvl + 1, max);
+    }
+    if (tree->right != NILOID) {
+        heightTreeOid(tree->right, lvl + 1, max);
+    }
+    return;
+}
+
 
 void heightTree(Node* tree, int lvl, int* max) {
     if (*max < lvl)
@@ -18,20 +30,34 @@ void heightTree(Node* tree, int lvl, int* max) {
     return;
 }
 
+
+int SumTreeRecurs(NodeOID* tree) {
+    if (tree == NILOID)
+        return 0;
+    return SumTreeRecurs(tree->left) + SumTreeRecurs(tree->right) + 1;
+}
+
+void printCountOID(NodeOID* tree) {
+    int sum = SumTreeRecurs(tree);
+    printf("count=%d\n", sum);
+    fflush(stdout);
+}
+
 int cancle_order(Node **glass, NodeOID *noid){
     Node* node = *glass;
     PriceData price_data = {noid->data.price};
-    Node *nd = findNode(&node, price_data);
-    if(nd == NULL) {
+    Node *node_to_cncl = findNode(&node, price_data);
+    if(node_to_cncl == NULL) {
         printf("Error: Node not contain price_level=%f for this oid=%d\n", noid->data.price, noid->data.oid);
         fflush(stdout);
         return 0;
     }
-    OrderLevel *ol = nd->data.price_level->head;
+    OrderLevel *ol = node_to_cncl->data.price_level->head;
     if(ol != NULL && ol->value->oid == noid->data.oid) {
-        OrderLevel *tmp = ol->next;
-        ol->next = ol->next->next;
+        OrderLevel *tmp = ol;
+        ol = ol->next;
         free(tmp);
+        node_to_cncl->data.price_level->size--;
     } else {
         while(ol->next != NULL) {
             if(ol->next->value->oid == noid->data.oid)
@@ -44,10 +70,11 @@ int cancle_order(Node **glass, NodeOID *noid){
             OrderLevel *tmp = ol->next;
             ol->next = ol->next->next;
             free(tmp);
+            node_to_cncl->data.price_level->size--;
         }
     }
-    if(nd->data.price_level->size == 0)
-        deleteNode(&node, nd);
+    if(node_to_cncl->data.price_level->size == 0)
+        deleteNode(&node, node_to_cncl);
     *glass = node;
     return 0;
 }
@@ -61,36 +88,44 @@ int main()
         exit(EXIT_FAILURE);
     }
     char c_type;
-    int uniq_number = 0;
+    int oid = 0;
     char c_side;
     int qty = 0;
     float price = 0.;
 
-    unsigned int trade_id = 0;
+    unsigned int trade_id = 1;
     Node* bye_glass = NIL;
     Node* sell_glass = NIL;
     NodeOID* oidstore = NILOID;
-    while(fscanf(in, "%c,%d,%c,%d,%f", &c_type, &uniq_number, &c_side, &qty, &price) != EOF) {
+
+
+    int counter = 0;
+    int counter_args = 0;
+    while((counter_args = fscanf(in, "%c,%d,%c,%d,%f", &c_type, &oid, &c_side, &qty, &price)) != EOF) {
+        if(counter_args == 1)
+            continue;
+//        if(counter == 22)
+//            break;
+
         PriceData *n = NULL;
         n = (PriceData*)malloc(sizeof (PriceData));
 
         n->price = price;
-        n->oid = uniq_number;
+        n->oid = oid;
         n->qty = qty;
         n->side = c_side;
         if(c_type == 'O') {
-            if(c_side == 'B') {
+            if(n->side == 'B') {
                 Node* tmp = sell_glass;
                 while(tmp->left != NIL)
                     tmp = tmp->left;
-                while(tmp->data.price >= price && tmp != NIL && n->qty != 0) {	// Если есть покупатели то выполняем сделки
-                    OrderLevel *orders = tmp->data.price_level->head;	// Получаем первый в очереди ордер на покупку
-                    while(orders != NULL) {	// Обходим очередь или выход из цикла по break
+                while(tmp->data.price <= n->price && tmp != NIL && n->qty != 0) {	// Если есть покупатели то выполняем сделки
+                    OrderLevel *orders = tmp->data.price_level->head;			// Получаем первый в очереди ордер на покупку
+                    while(orders != NULL) {				// Обходим очередь или выход из цикла по break
                         if(orders->value->qty > n->qty) {
                             // Ордер который прилетел закрыт
                             orders->value->qty -= n->qty;
                             printf("T,%u,%c,%d,%d,%d,%.2f\n", trade_id++, 'S', orders->value->oid, n->oid, n->qty, orders->value->price);
-                            insertNodeOID(&oidstore, (OID){n->oid, n->price, n->side});
                             n->qty = 0;
                             break;
                         } else if(orders->value->qty < n->qty) {
@@ -121,7 +156,7 @@ int main()
                     }
                 }
                 if(n->qty != 0) {
-                    insertNodeOID(&oidstore, (OID){n->oid, n->side});
+                    insertNodeOID(&oidstore, (OID){n->oid, n->price, n->side});
                     insertNode(&bye_glass, *n);
                 }
             } else {
@@ -129,14 +164,13 @@ int main()
                 Node* tmp = bye_glass;	// самая высокая цена на покупку
                 while(tmp->right != NIL)
                     tmp = tmp->right;
-                while(tmp->data.price >= price && tmp != NIL && n->qty != 0) {	// Если есть покупатели то выполняем сделки
+                while(tmp->data.price >= n->price && tmp != NIL && n->qty != 0) {	// Если есть покупатели то выполняем сделки
                     OrderLevel *orders = tmp->data.price_level->head;	// Получаем первый в очереди ордер на покупку
                     while(orders != NULL) {	// Обходим очередь или выход из цикла по break
                         if(orders->value->qty > n->qty) {
                             // Ордер который прилетел закрыт
                             orders->value->qty -= n->qty;
                             printf("T,%u,%c,%d,%d,%d,%.2f\n", trade_id++, 'B', orders->value->oid, n->oid, n->qty, orders->value->price);
-                            insertNodeOID(&oidstore, (OID){n->oid, n->price, n->side});
                             n->qty = 0;
                             break;
                         } else if(orders->value->qty < n->qty) {
@@ -148,7 +182,7 @@ int main()
                             // Ордер который был в стакане закрыт
                         } else {
                             printf("T,%u,%c,%d,%d,%d,%.2f\n", trade_id++, 'B', orders->value->oid, n->oid, orders->value->qty, orders->value->price);
-                            insertNodeOID(&oidstore, (OID){orders->value->oid, n->price, orders->value->side});
+                            insertNodeOID(&oidstore, (OID){orders->value->oid + 2, n->price, orders->value->side});
                             pop_front(tmp->data.price_level);
                             orders = tmp->data.price_level->head;
                             n->qty = 0;
@@ -167,7 +201,7 @@ int main()
                     }
                 }
                 if(n->qty != 0) {
-                    insertNodeOID(&oidstore, (OID){n->oid, n->side});
+                    insertNodeOID(&oidstore, (OID){n->oid, n->price, n->side});
                     insertNode(&sell_glass, *n);
                 }
             }
@@ -186,21 +220,29 @@ int main()
             } else {
                 cancle_order(&sell_glass, noid);
             }
+            //printCountOID(oidstore);
             deleteNodeOID(&oidstore, noid);
+            //printCountOID(oidstore);
         }
+        fflush(stdout);
+        counter++;
+        //n = (PriceData*)malloc(sizeof (PriceData));
     }
 
     int bye_max = 0;
     int sell_max = 0;
+    int oid_max = 0;
     heightTree(bye_glass, 0, &bye_max);
     heightTree(sell_glass, 0, &sell_max);
+    heightTreeOid(oidstore, 0, &oid_max);
     PriceData *p = (PriceData*)malloc(sizeof(PriceData));
     p->price = 250.61;
-    Node *n = NIL;
-    n = findNode(&bye_glass, *p);
-    if(n != NULL)
-        printf("%f\n",n->data.price);
+    Node *ntest = NIL;
+    ntest = findNode(&bye_glass, *p);
+    if(ntest != NULL)
+        printf("%f\n",ntest->data.price);
     printf("\nMax_bye=%d\tmax_sell=%d \nEnd of file!\n", bye_max, sell_max);
+    printf("max_height_oid=%d\n", oid_max);
     fclose(in);
 
     return 0;
