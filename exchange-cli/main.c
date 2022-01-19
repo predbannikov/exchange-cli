925f812af30d3cd94b377c7f8480a085831c2f1c
Yurok <predbannikov.yurii@gmail.com>
2022-01-19 13:17:50 +0300

    described main.c ver1.3

--- exchange-cli/main.c
+++ exchange-cli/main.c
@@ -1,21 +1,133 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Node.h"
+#include "node.h"
 
+/* *************************************************
+ * использовать рабочую на данном датасете функцию *
+ * вывода информации в файл                        *
+ * *************************************************/
 #define CUSTOM_PRINT
+/* тест на чтение с буфера и обработка алгоритмом */
+//#define TEST_CYCLE	1000
 
 unsigned int trade_id = 1;
-int left_dgt;
-int right_dgt;
-char buffer[100];
-int size;
-int tmp;
-FILE *out;
+int left_dgt = 0;
+int right_dgt = 0;
+char buffer[64];
+char ch;
+int szbuff = 0;
+int tmp = 0;
+FILE *in = NULL, *out = NULL;
 
-enum {STATE_START, STATE_OID, STATE_SIDE, STATE_QTY, STATE_PRICE, STATE_PRICE2, STATE_END} state_parser = STATE_START;
+/* состояния для парсинга входящей строки аналог get_args */
+enum {STATE_START, STATE_OID, STATE_SIDE, STATE_QTY, STATE_PRICE_LEFT, STATE_PRICE_RIGHT } state_parser = STATE_START;
 enum {STATE_ORDER, STATE_CANCLE} state_ticket = STATE_ORDER;
-char shift_dgt = '0';
 
+/* парсинг входящей строки */
+void get_args(unsigned int *oid, char *side, unsigned int *qty, float *price) {
+    *oid = 0;
+    *qty = 0;
+    left_dgt = 0;
+    right_dgt = 0;
+    int cnt_right_dgt = 0;
+    switch (state_ticket) {
+    case STATE_ORDER:
+        while((ch = getc(in)) != EOF && ch != '\n') {
+            if(ch == ',') {
+                state_parser++;
+                continue;
+            }
+            switch (state_parser) {
+            case STATE_START:
+                break;
+            case STATE_OID:
+                *oid *= 10;
+                *oid += (ch - '0');
+                break;
+            case STATE_SIDE:
+                *side = ch;
+                break;
+            case STATE_QTY:
+                *qty *= 10;
+                *qty += (ch - '0');
+                break;
+            case STATE_PRICE_LEFT:
+                if(ch=='.') {
+                    state_parser = STATE_PRICE_RIGHT;
+                    break;
+                }
+                left_dgt *= 10;
+                left_dgt += (ch - '0');
+                break;
+            case STATE_PRICE_RIGHT:
+                cnt_right_dgt++;
+                right_dgt *= 10;
+                right_dgt += (ch - '0');
+                break;
+            }
+        }
+        state_parser = STATE_START;
+        break;
+    case STATE_CANCLE:
+        while((ch = getc(in)) != EOF && ch != '\n') {
+            if(ch == ',')
+                continue;
+            *oid *= 10;
+            *oid += (ch - '0');
+        }
+        break;
+    }
+    if(cnt_right_dgt == 1)
+        *price = left_dgt + right_dgt/10.;
+    else
+        *price = left_dgt + right_dgt/100.;
+}
+
+/* кастомная версия fprintf с совершённой сделкой */
+void print_trade(unsigned int oid, unsigned int oid2, unsigned int qty, char side, float price) {
+
+    putc('T', out);
+    putc(',', out);
+
+    tmp = trade_id++;
+    do buffer[szbuff++] = tmp % 10 + '0'; while ((tmp /= 10) > 0);
+    while (szbuff > 0)
+        putc(buffer[--szbuff], out);
+    putc(',', out);
+    putc(side, out);
+    putc(',', out);
+
+    left_dgt = price + 0.005;
+    right_dgt = ((price - left_dgt + 0.005) * 100);
+    if(right_dgt % 10 == 0)
+        right_dgt /= 10;
+    do buffer[szbuff++] = right_dgt % 10 + '0'; while ((right_dgt /= 10) > 0);
+    buffer[szbuff++] = '.';
+    do buffer[szbuff++] = left_dgt % 10 + '0'; while ((left_dgt /= 10) > 0);
+    buffer[szbuff++] = ',';
+    do buffer[szbuff++] = qty % 10 + '0'; while ((qty /= 10) > 0);
+    buffer[szbuff++] = ',';
+    do buffer[szbuff++] = oid2 % 10 + '0'; while ((oid2 /= 10) > 0);
+    buffer[szbuff++] = ',';
+    do buffer[szbuff++] = oid % 10 + '0'; while ((oid /= 10) > 0);
+
+    while (szbuff > 0)
+        putc(buffer[--szbuff], out);
+    putc('\n', out);
+}
+
+/* кастомная версия fprintf с отменённым ордером */
+void print_cancle(unsigned int oid) {
+    putc('X', out);
+    putc(',', out);
+    do buffer[szbuff++] = oid % 10 + '0';
+    while ((oid /= 10) > 0);
+    while (szbuff > 0)
+        putc(buffer[--szbuff], out);
+   putc('\n', out);
+}
+
+/* очистить дерево прайсов с ордерами */
 void free_nodes(Node* tree) {
     while(tree != NIL) {
         PriceLevel* price_level = tree->price_level;
@@ -30,17 +142,18 @@ void free_nodes(Node* tree) {
     }
 }
 
+/* очистить дерево OID с прайсами */
 void free_oidstore(NodeOID* tree) {
     while(tree != NILOID)
         deleteNodeOID(&tree, tree);
 }
 
+/* отмена ордера */
 int cancle_order(Node **glass, NodeOID *noid){
     Node* node = *glass;
     Node *node_to_cncl = findNode(&node, noid->data.price);
-    if(node_to_cncl == NULL) {
+    if(node_to_cncl == NULL)
         return 0;
-    }
 
     delete_Nth_of_oid(node_to_cncl->price_level, noid->data.oid);
 
@@ -50,6 +163,7 @@ int cancle_order(Node **glass, NodeOID *noid){
     return 0;
 }
 
+/* получить наименьший/наибольший прайс ордеров */
 Node* extr_elem(Node* tree, char side) {
     Node *tmp = tree;
     if(side == 'B') {
@@ -62,67 +176,7 @@ Node* extr_elem(Node* tree, char side) {
     return tmp;
 }
 
-void print_trade(unsigned int oid, unsigned int oid2, unsigned int qty, char side, float price) {
-    left_dgt = (int) price + 0.005;
-    right_dgt = (int)((price - left_dgt + 0.005) * 100);
-    if(right_dgt % 10 == 0)
-        right_dgt /= 10;
-    size = 0;
-
-    do {
-        buffer[size++] = right_dgt % 10 + '0';
-    } while ((right_dgt /= 10) > 0);
-    buffer[size++] = '.';
-
-    do {
-        buffer[size++] = left_dgt % 10 + '0';
-    } while ((left_dgt /= 10) > 0);
-    buffer[size++] = ',';
-
-    do {
-        buffer[size++] = qty % 10 + '0';
-    } while ((qty /= 10) > 0);
-
-    buffer[size++] = ',';
-    do {
-        buffer[size++] = oid2 % 10 + '0';
-    } while ((oid2 /= 10) > 0);
-    buffer[size++] = ',';
-
-    do {
-        buffer[size++] = oid % 10 + '0';
-    } while ((oid /= 10) > 0);
-
-    buffer[size++] = ',';
-    buffer[size++] = side;
-
-    buffer[size++] = ',';
-    tmp = trade_id++;
-    do {
-        buffer[size++] = tmp % 10 + '0';
-    } while ((tmp /= 10) > 0);
-
-    putc('T', out);
-    putc(',', out);
-
-    for(int j = size - 1; j >= 0; j--)
-        putc(buffer[j], out);
-
-    putc('\n', out);
-}
-
-void print_cancle(unsigned int oid) {
-    size = 0;
-    putc('X', out);
-    putc(',', out);
-    do {
-        buffer[size++] = oid % 10 + '0';
-    } while ((oid /= 10) > 0);
-    for(int j = size - 1; j >= 0; j--)
-        putc(buffer[j], out);
-   putc('\n', out);
-}
-
+/* произвести сделки ордеров при новопоступившем ордере*/
 void matching(Node** glass, Order *order, NodeOID **oidstr) {
 
     Node* border_elem = extr_elem(*glass, order->side);
@@ -196,90 +250,23 @@ void matching(Node** glass, Order *order, NodeOID **oidstr) {
     }
 }
 
-int get_args(unsigned int *oid, char *side, unsigned int *qty, float *price, FILE *in) {
-    *oid = 0;
-    *qty = 0;
-    int pc1 = 0, pc2 = 0;
-    char ch;
-    int cnt_right_dgt = 0;
-    switch (state_ticket) {
-    case STATE_ORDER:
-        while((ch = getc(in)) != EOF && ch != '\n') {
-            if(ch == ',') {
-                state_parser++;
-                continue;
-            }
-            switch (state_parser) {
-            case STATE_START:
-                break;
-            case STATE_OID:
-                *oid *= 10;
-                *oid += (ch - shift_dgt);
-                break;
-            case STATE_SIDE:
-                *side = ch;
-                break;
-            case STATE_QTY:
-                *qty *= 10;
-                *qty += (ch - shift_dgt);
-                break;
-            case STATE_PRICE:
-                if(ch=='.') {
-                    state_parser = STATE_PRICE2;
-                    break;
-                }
-                pc1 *= 10;
-                pc1 += (ch - shift_dgt);
-                break;
-            case STATE_PRICE2:
-                if(ch == '\n') {
-                    state_parser = STATE_END;
-                    break;
-                }
-                cnt_right_dgt++;
-                pc2 *= 10;
-                pc2 += (ch - shift_dgt);
-                break;
-            case STATE_END:
-                break;
-            }
-        }
-        state_parser = STATE_START;
-        break;
-    case STATE_CANCLE:
-        while((ch = getc(in)) != EOF && ch != '\n') {
-            if(ch == ',')
-                continue;
-            *oid *= 10;
-            *oid += (ch - shift_dgt);
-        }
-        break;
-    }
-    if(cnt_right_dgt == 1)
-        *price = pc1 + pc2/10.;
-    else
-        *price = pc1 + pc2/100.;
-
-
-    return 0;
-}
-
-void exchange(FILE *in) {
-    char c_type;
+/* цикл обработки полученных ордеров и вставки их в очередь */
+void exchange() {
     Node* bye_glass = NIL;
     Node* sell_glass = NIL;
     NodeOID* oidstore = NILOID;
     Order *order = NULL;
     order = (Order*)malloc(sizeof (Order));
-
-    char ch;
-
+    if (order == NULL) {
+        fprintf(stderr, "error: insufficient memory (exchange)\n");
+        exit(EXIT_FAILURE);
+    }
     while((ch = getc(in)) != EOF) {
         if(ch == 'O')
             state_ticket = STATE_ORDER;
         else
             state_ticket = STATE_CANCLE;
-        get_args(&order->oid, &order->side, &order->qty, &order->price, in);
+        get_args(&order->oid, &order->side, &order->qty, &order->price);
 
         if(state_ticket == STATE_ORDER) {
             if(order->side == 'B') {
@@ -300,6 +287,10 @@ void exchange(FILE *in) {
                 }
             }
             order = (Order*)malloc(sizeof (Order));
+            if (order == NULL) {
+                fprintf(stderr, "error: insufficient memory (exchange)\n");
+                exit(EXIT_FAILURE);
+            }
         } else if(state_ticket == STATE_CANCLE) {
             NodeOID *noid = findNodeOID(&oidstore, order->oid);
             if(noid == NULL)
@@ -328,14 +319,17 @@ void exchange(FILE *in) {
 
 int main(int argc, char **argv)
 {
-    FILE *in;
     if(argc > 1 && argc < 3) {
         in = fopen(argv[1], "rt");
         if(in == NULL ) {
             fprintf(stderr, "Error openning file to read <%s>.\n", argv[1]);
             exit(EXIT_FAILURE);
         }
+#ifdef TEST_CYCLE
+        out = fopen("/dev/null", "wt");
+#else
         out = fopen("out.txt", "wt");
+#endif
         if(out == NULL) {
             fprintf(stderr, "Error opening file to write out.txt\n");
             exit(EXIT_FAILURE);
@@ -344,10 +338,14 @@ int main(int argc, char **argv)
         fprintf(stderr, "Missing argument <input_file>.\n");
         exit(EXIT_FAILURE);
     }
-    for(int i = 0; i < 1; i++) {
+#ifdef TEST_CYCLE
+    for(int i = 0; i < TEST_CYCLE; i++) {
         fseek(in, 0, SEEK_SET);
-        exchange(in);
+        exchange();
     }
+#else
+    exchange();
+#endif
     fclose(in);
     fclose(out);
     return EXIT_SUCCESS;
